9fecae0dec5988731218e7b9564e30968e69e876
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2020-03-13 02:09:18 +0200

    add: user, statistics model

---
+++ functions/src/models/user-statistics.ts
@@ -0,0 +1,11 @@
+export interface UserStatistics {
+    clicks: number;
+    date: string;
+    page_views: number;
+    user_id: number;
+}
+
+export interface BasicStatistics {
+    clicks: number;
+    user_id: number;
+}
---
+++ functions/src/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    email: string;
+    first_name: string;
+    gender: 'Male' | 'Female';
+    id: number;
+    ip_address: string;
+    last_name: string;
+}
